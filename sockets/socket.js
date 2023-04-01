94d8197bd87e516a9ed3475686b2dfa71add65a2
IDaMariusI <fcovillanueva1988@gmail.com>
2023-04-01 13:42:09 -0600

    34. Socket: Emitir bandas registradas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,19 @@
 const {io} = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del Silencio'));
+bands.addBand(new Band('Metallica'));
 
 //Sockets Messages
 io.on('connection', client => {
     console.log('Client Conected');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Client Disconected');
