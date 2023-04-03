52a2bcdcc508b91a5550fb200d4b91a7d28d3703
IDaMariusI <fcovillanueva1988@gmail.com>
2023-04-03 15:30:56 -0600

    37. Socket: Agregar una nueva Banda

--- sockets/socket.js
+++ sockets/socket.js
@@ -28,4 +28,10 @@ io.on('connection', client => {
         bands.voteBand(payload.id);
         io.emit('active-bands', bands.getBands());
     });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
   });
